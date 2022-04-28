9a0229d7e8ad635786f3954ab0e1f101769de444
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-04-28 23:42:11 +0200

    пуш проміжного варіанту  Завдання 1

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+
+function createGalleryItemMarkup(galleryItems) {
+    const markup = galleryItems.map(({preview ,original,description}) => {
+        return `
+        <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>
+    `;
+    }).join('');
+    
+    return markup;
+}
+
+function onGalleryContainerClick(event) {
+    if (!event.target.classlist.contains('gallery__image')) {
+        return;
+    }
+
+    console.log(event.target)
+}
